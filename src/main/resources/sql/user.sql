011f8d6902ca4d7071b4adae52b248deaae8f027
yanghe <yanghe@datangmobile.cn>
2022-03-04 17:39:17 +0800

    增加测试类

--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -1,2 +1,39 @@
-INSERT INTO `user` (`id`, `user_name`, `user_sex`, `user_address`) VALUES (1, 'yanghe', 'M', '北京市海淀区');
-INSERT INTO `user` (`id`, `user_name`, `user_sex`, `user_address`) VALUES (2, 'yanghe1', 'M', '北京市东城区');
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 192.168.201.10
+ Source Server Type    : MySQL
+ Source Server Version : 80027
+ Source Host           : 192.168.201.10:3306
+ Source Schema         : demo
+
+ Target Server Type    : MySQL
+ Target Server Version : 80027
+ File Encoding         : 65001
+
+ Date: 04/03/2022 17:38:40
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `user_sex` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
+  `user_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (8, '小白', 'M', '北京市西城区', '2022-03-04 09:36:27', '2022-03-04 09:37:18');
+
+SET FOREIGN_KEY_CHECKS = 1;
